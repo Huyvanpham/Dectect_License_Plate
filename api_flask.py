4448b429c14546e4f42d630c51f8c7e497f3df77
Huyvanpham <huyvanphambg@gmail.com>
2025-01-06 14:37:08 +0700

    updated

--- api_flask.py
+++ api_flask.py
@@ -1,66 +1,83 @@
 from flask import Flask, jsonify, request, send_file
 import datetime
 import requests
-from lp_video import hanleVideo
+from lp_video import handleVideo
 import qrcode
 import io
+import subprocess
 
 
 app = Flask(__name__)
 
+
+def message(type, message=""):
+    subprocess.Popen(["python", "test.py", type, message])
 
 @app.route("/api/get_status", methods=["POST"])
 def getStatus():
     file = request.files['file']
     action = request.form['action']
     path =  datetime.datetime.now().strftime("%Y-%m-%d") +".mp4"
-    file.save(f'./test_video/{path}')
-    check = hanleVideo(f'./test_video/{path}')
+    file.save(f'./result/{path}')
+    check = handleVideo(f'./result/{path}')
 
     if check[0] is None:
         return jsonify({"code": "400", "status":"Khong nhan dien duoc bien so"}), 200
 
+    code = check[0]
+
+    for i in check:
+        if len(i) > len(code):
+            code = i
+
     payload = {
-        "code":check[0],
-        "action":action,
+        "code": code,
+        "action": action,
     }
 
     header = {
-        "Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE3MzYwMTIzMTksImV4cCI6MTczNjAxNTkxOX0.wU5etsKxUiSq9yFj0bFCEvl4Yvu-EuK6g-f9a5B2nic"
+        "Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE3MzYwNTU1ODF9.b5yQEIxqeb6JEaJTuFYpPJVzCQXkl8JyMyJ_uXuWgBI"
     }
 
     res = requests.post("https://respective-lynnell-quangtx-65b507bd.koyeb.app/action-history", json=payload, headers=header)
-    res =res.json()
+    res = res.json()
+    print(res)
     if res['status'] == False:
-        return jsonify({"message":res['message'], "code":res['responseCode']})
+        if res['responseCode'] == 400: 
+            message("message", res['message'])
+            return jsonify({"message":res['message']})
+        return jsonify({"message":"server error"}), 500
 
-    url = res['data']['url']
+    if res['data']['action'] == "OPEN":
+        message("message", "Cửa đã mở")
+        return jsonify({"code": "200", "status":"Cua da mo"}), 200
+    else:
+        url = res['data']['url']
 
-    if not url:
-        return jsonify({"code":"500", "status": "Khong co url"}), 200
-    # Tạo QR code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
+        if not url:
+            return jsonify({"code":"500", "status": "Khong co url"}), 200
+        
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=3,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
 
-    # Lưu QR code vào bộ nhớ
-    img = qr.make_image(fill_color="black", back_color="white")
-    buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
-    buffer.seek(0)
-    return send_file(buffer, mimetype='image/png')
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+        return send_file(buffer, mimetype='image/png')
 
 
 
 @app.route("/api/open_door", methods=["GET"])
 def openDoor():
-    print("Cua da mo")
-    return jsonify({"code": "200", "status":"12312312"}), 200
+    message("message", "Cửa đã mở")
+    return jsonify({"message": "success!"}), 200
 
 
 #app.run(host='0.0.0.0', port=5111, debug=True)
--- lp_video.py
+++ lp_video.py
@@ -1,79 +1,93 @@
 from PIL import Image
 import cv2
 import torch
-import math 
 import function.utils_rotate as utils_rotate
 from IPython.display import display
-import os
-import time
-import argparse
 import function.helper as helper
+import re
 
-# Chấp nhận đường dẫn video thay vì ảnh
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--video', required=True, help='path to input video')
-args = ap.parse_args()
 
-# Tải mô hình YOLOv5 cho nhận diện biển số và OCR
 yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector.pt', force_reload=True, source='local')
 yolo_license_plate = torch.hub.load('yolov5', 'custom', path='model/LP_ocr.pt', force_reload=True, source='local')
 yolo_license_plate.conf = 0.60
 
-# Mở video
-cap = cv2.VideoCapture(args.video)
+def formatCode(bien_so):
+    bien_so = bien_so.replace(" ", "")
+    bien_so = bien_so.upper()
+    bien_so = bien_so.replace("-", "")
+    pattern = re.compile(r"([A-Z])(?=\d)") 
+    bien_so = pattern.sub(r"\1-", bien_so) 
+    return bien_so
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        print("Không thể đọc frame từ video.")
-        break
+def handleVideo(video_path):
 
-    plates = yolo_LP_detect(frame, size=640)
-    list_plates = plates.pandas().xyxy[0].values.tolist()
-    list_read_plates = set()
+    cap = cv2.VideoCapture(video_path)
+    listCode = []
 
-    if len(list_plates) == 0:
-        lp = helper.read_plate(yolo_license_plate, frame)
-        if lp != "unknown":
-            cv2.putText(frame, lp, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-            list_read_plates.add(lp)
-    else:
-        for plate in list_plates:
-            flag = 0
-            x = int(plate[0]) # xmin
-            y = int(plate[1]) # ymin
-            w = int(plate[2] - plate[0]) # xmax - xmin
-            h = int(plate[3] - plate[1]) # ymax - ymin  
-            crop_img = frame[y:y+h, x:x+w]
-            cv2.rectangle(frame, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
-            cv2.imwrite("crop.jpg", crop_img)
-            rc_image = cv2.imread("crop.jpg")
-            lp = ""
-            for cc in range(0,2):
-                for ct in range(0,2):
-                    lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
-                    if lp != "unknown":
-                        list_read_plates.add(lp)
-                        cv2.putText(frame, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                        flag = 1
+    checkEnd = False
+    rep = int(0)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Không thể đọc frame từ video.")
+            break
+
+        plates = yolo_LP_detect(frame, size=640)
+        list_plates = plates.pandas().xyxy[0].values.tolist()
+        list_read_plates = set()
+
+        if len(list_plates) == 0:
+            lp = helper.read_plate(yolo_license_plate, frame)
+            if lp != "unknown":
+                cv2.putText(frame, lp, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                list_read_plates.add(lp)
+        else:
+            for plate in list_plates:
+                flag = 0
+                x = int(plate[0]) 
+                y = int(plate[1])
+                w = int(plate[2] - plate[0])
+                h = int(plate[3] - plate[1])
+                crop_img = frame[y:y+h, x:x+w]
+                cv2.rectangle(frame, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
+                cv2.imwrite("crop.jpg", crop_img)
+                rc_image = cv2.imread("crop.jpg")
+                lp = ""
+                for cc in range(0,2):
+                    for ct in range(0,2):
+                        lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
+                        if lp != "unknown":
+                            list_read_plates.add(lp)
+                            cv2.putText(frame, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                            flag = 1
+                            break
+                    if flag == 1:
                         break
-                if flag == 1:
-                    break
-    
-    # In biển số ra dạng văn bản
-    if list_read_plates:
-        print("Biển số xe vào bãi:", list(list_read_plates))
         
-    # Hiển thị kết quả
-    width, height = 800, 800  # Kích thước mong muốn
-    resized_frame = cv2.resize(frame, (width, height))
+        if list_read_plates and rep < 5:
+            checkEnd = True
+            rep += 1
 
-    cv2.imshow('frame', resized_frame)
+            code = list(list_read_plates)[0]
+            code = formatCode(code)
 
-    # Nhấn 'q' để thoát khi xem video
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            print("Biển số xe vào bãi:", code)
+            listCode.append(code)
+        else:
+            if checkEnd:
+                cap.release()
+                cv2.destroyAllWindows()
+            
+        width, height = 800, 800 
+        resized_frame = cv2.resize(frame, (width, height))
 
-# Giải phóng tài nguyên khi kết thúc
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('frame', resized_frame)
+        cv2.setWindowProperty('frame', cv2.WND_PROP_TOPMOST, 1)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return listCode
+
